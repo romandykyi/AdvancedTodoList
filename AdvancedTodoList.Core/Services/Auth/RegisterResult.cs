0969a6c40315c79e03c26abf892b77635078e2f0
romandykyi <roman.dykyi.8210@gmail.com>
2024-02-16 14:03:04 +0100

    Create auth service interface

---
+++ AdvancedTodoList.Core/Services/Auth/RegisterResult.cs
@@ -0,0 +1,35 @@
+﻿namespace AdvancedTodoList.Core.Services.Auth;
+
+/// <summary>
+/// Class that represents a result of register operation.
+/// </summary>
+public class RegisterResult
+{
+	/// <summary>
+	/// Flag which determines whether result reports succeess.
+	/// </summary>
+	public bool IsSuccess { get; private init; }
+	/// <summary>
+	/// Error message.
+	/// </summary>
+	public string? Error { get; private init; }
+
+	private RegisterResult() { }
+
+	/// <summary>
+	/// Returns a result that reports success.
+	/// </summary>
+	/// <returns>
+	/// A result that reports success.
+	/// </returns>
+	public static RegisterResult Success() => new() { IsSuccess = true };
+
+	/// <summary>
+	/// Returns a result that reports failure.
+	/// </summary>
+	/// <param name="error">Error message.</param>
+	/// <returns>
+	/// A result that reports failure.
+	/// </returns>
+	public static RegisterResult Failure(string error) => new() { IsSuccess = false, Error = error };
+}
