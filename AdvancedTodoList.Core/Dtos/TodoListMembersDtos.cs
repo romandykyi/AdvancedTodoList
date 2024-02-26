a1ad80f68e3305fd3b703589d6b95dcb7876a357
romandykyi <roman.dykyi.8210@gmail.com>
2024-02-26 21:09:11 +0100

    Add DTOs for members and roles

---
+++ AdvancedTodoList.Core/Dtos/TodoListMembersDtos.cs
@@ -0,0 +1,16 @@
+﻿namespace AdvancedTodoList.Core.Dtos;
+
+/// <summary>
+/// DTO to view a member and their role.
+/// </summary>
+public record TodoListMemberPreviewDto(ApplicationUserPreviewDto Member, TodoListRolePreviewDto Role);
+
+/// <summary>
+/// DTO for adding a member to a to-do list.
+/// </summary>
+public record TodoListMemberAddDto(string UserId, int? RoleId = null);
+
+/// <summary>
+/// DTO for updating to-do list member's role.
+/// </summary>
+public record TodoListMemberUpdateRoleDto(int? RoleId);
---
+++ AdvancedTodoList.Core/Dtos/TodoListRolesDtos.cs
@@ -0,0 +1,23 @@
+﻿using AdvancedTodoList.Core.Models.TodoLists.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTodoList.Core.Dtos;
+
+/// <summary>
+/// Represents a partial view for a role.
+/// </summary>
+public record TodoListRolePreviewDto(int Id, string Name);
+
+/// <summary>
+/// Represents a full view for a role.
+/// </summary>
+public record TodoListRoleViewDto(int Id, string Name, RolePermissions Permissions);
+
+/// <summary>
+/// DTO for creating/updating a role.
+/// </summary>
+public record TodoListRoleCreateDto(string Name, RolePermissions Permissions);
