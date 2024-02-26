2c732ee15db0fbaadfa53ce7905a29fb4d90d8ea
romandykyi <roman.dykyi.8210@gmail.com>
2024-02-26 20:13:24 +0100

    Add DTOs for users

---
+++ AdvancedTodoList.Core/Dtos/ApplicationUserDtos.cs
@@ -0,0 +1,13 @@
+﻿using AdvancedTodoList.Core.Models.Auth;
+
+namespace AdvancedTodoList.Core.Dtos;
+
+/// <summary>
+/// Represents a minimal view DTO for <see cref="ApplicationUser" />
+/// </summary>
+public record ApplicationUserPreviewDto(string Id, string UserName);
+
+/// <summary>
+/// Represents a view DTO for <see cref="ApplicationUser" />
+/// </summary>
+public record ApplicationUserViewDto(string Id, string Email, string UserName, string FirstName, string LastName);
