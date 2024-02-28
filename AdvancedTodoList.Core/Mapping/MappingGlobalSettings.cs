1bb01210a931e79ba145902a0191b4431f16463d
romandykyi <roman.dykyi.8210@gmail.com>
2024-02-28 20:03:35 +0100

    Fix mapping issue

--- AdvancedTodoList.Core/Mapping/MappingGlobalSettings.cs
+++ AdvancedTodoList.Core/Mapping/MappingGlobalSettings.cs
@@ -1,4 +1,7 @@
-﻿using Mapster;
+﻿using AdvancedTodoList.Core.Dtos;
+using AdvancedTodoList.Core.Models.TodoLists.Members;
+using Mapster;
+using System.Text.RegularExpressions;
 
 namespace AdvancedTodoList.Core.Mapping;
 
@@ -12,6 +15,10 @@ public static class MappingGlobalSettings
 	/// </summary>
 	public static void Apply()
 	{
+		// Ignore null IDs
+		TypeAdapterConfig<TodoListMember, TodoListMemberPreviewDto>.NewConfig()
+			.IgnoreIf((src, dest) => src.RoleId == null, dest => dest.Role!);
+
 		// Convert null strings into empty strings and trim strings
 		TypeAdapterConfig.GlobalSettings.Default
 			.AddDestinationTransform((string? dest) => dest != null ? dest.Trim() : string.Empty);
