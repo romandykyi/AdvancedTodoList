0fd054c3fd501992cafb8164835becae1d7ab65c
romandykyi <roman.dykyi.8210@gmail.com>
2024-03-30 17:58:16 +0100

    Implement invitation links specification

---
+++ AdvancedTodoList.Infrastructure/Specifications/InvitationLinksSpecification.cs
@@ -0,0 +1,32 @@
+﻿using AdvancedTodoList.Core.Models.TodoLists;
+using AdvancedTodoList.Core.Specifications;
+using System.Linq.Expressions;
+
+namespace AdvancedTodoList.Infrastructure.Specifications;
+
+/// <summary>
+/// Represents a specification that defines criteria for filtering invitation links.
+/// </summary>
+/// <param name="todoListId">ID of the list invitation links of which will be obtained.</param>
+public class InvitationLinksSpecification(string todoListId) : ISpecification<InvitationLink>
+{
+	/// <summary>
+	/// Gets the ID of the to-do list to filter entities by.
+	/// </summary>
+	public string? TodoListId { get; } = todoListId;
+
+	/// <summary>
+	/// Gets the criteria expression that defines the filtering conditions.
+	/// </summary>
+	public virtual Expression<Func<InvitationLink, bool>> Criteria => x => x.TodoListId == TodoListId;
+
+	/// <summary>
+	/// Gets the list of include expressions specifying related entities to be included in the query results.
+	/// </summary>
+	public virtual List<Expression<Func<InvitationLink, object?>>> Includes { get; init; } = [];
+
+	/// <summary>
+	/// Gets the list of include strings specifying related entities to be included in the query results.
+	/// </summary>
+	public virtual List<string> IncludeStrings { get; init; } = [];
+}
---
+++ AdvancedTodoList.UnitTests/Specifications/InvitationLinksSpecificationTests.cs
@@ -0,0 +1,40 @@
+﻿using AdvancedTodoList.Core.Models.TodoLists;
+using AdvancedTodoList.Infrastructure.Specifications;
+
+namespace AdvancedTodoList.UnitTests.Specifications;
+
+[TestFixture]
+public class InvitationLinksSpecificationTests
+{
+	[Test]
+	public void Criteria_TodoListIdMatches_ReturnsTrue()
+	{
+		// Arrange
+		const string todoListId = "ID";
+		InvitationLink link = new() { TodoListId = todoListId, Value = "link" };
+		InvitationLinksSpecification specification = new(todoListId);
+		var function = specification.Criteria.Compile();
+
+		// Act
+		bool result = function(link);
+
+		// Assert
+		Assert.That(result, Is.True);
+	}
+
+	[Test]
+	public void Criteria_TodoListIdDoesNotMatch_ReturnsFalse()
+	{
+		// Arrange
+		const string todoListId = "ID";
+		InvitationLink link = new() { TodoListId = todoListId, Value = "link" };
+		InvitationLinksSpecification specification = new("Wrong ID");
+		var function = specification.Criteria.Compile();
+
+		// Act
+		bool result = function(link);
+
+		// Assert
+		Assert.That(result, Is.False);
+	}
+}
